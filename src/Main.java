ab3d569ec4cb02529b400574d0d0a97b203e223b
aliamanuba <aliamanuba@leafstudiosdot.co.uk>
2025-04-03 08:21:32 +0800

    add: MySQL Connection

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String[] dbconn = {"jdbc:mysql://localhost:3306/finals", "username", "password"};
+
+        System.out.println("Connecting to MySQL database...");
+
+        try (Connection connection = DriverManager.getConnection(dbconn[0], dbconn[1], dbconn[2])) {
+            System.out.println("Database connected!");
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database", e);
+        }
     }
 }
